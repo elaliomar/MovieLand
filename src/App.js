e6b2eec9be588ec6c2e9a817fc111028b5b44507
elaliomar <elali-omar@outlook.com>
2023-02-22 12:29:18 -0800

    MovieLand Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import './App.css'
+import SearchIcon from './search.svg'
+import MovieCard from "./MovieCard";
 
-function App() {
+const API_URL='http://www.omdbapi.com?apikey=d78e46c5';
+function App  () {
+  const [movies,setMovies]=useState([]);
+  const [SearchTerm,setSearchTerm] =useState('');
+  const searchMovies = async(title)=>{
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json();
+    setMovies(data.Search)
+   
+  }
+ 
+  const movie1 =
+    {
+      "Title": "The Amazing Spiderman 2 Webb Cut",
+      "Year": "2021",
+      "imdbID": "tt18351128",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BYzYzZDViNWYtNWViMS00NDMxLThlN2YtZjFkOWMwODkzNzhiXkEyXkFqcGdeQXVyMTUwMzM4NzU0._V1_SX300.jpg"
+  }
+  
+
+  useEffect(()=>{
+     searchMovies('Spiderman');
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input 
+          placeholder="Search for movies"
+          value={SearchTerm}
+          onChange={(e)=>{setSearchTerm(e.target.value)}}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={()=>{searchMovies(SearchTerm)}}
+        />
+      </div>
+      {
+        movies.length > 0 
+        ?(
+          <div className="container">
+            {movies.map((movie)=>(
+              <MovieCard movie={movie}/>
+            ))}
+          
+        </div>
+        ):(
+          <div className="empty">
+            <h2>No movies found</h2>
+            </div>
+
+        )
+      }
+     
     </div>
   );
 }
---
+++ src/Person.js
@@ -0,0 +1,10 @@
+const Person = (props) => {
+    return (  
+        <div>
+            <p>Name: {props.name}</p>
+            <p>LastName:El Ali</p>
+        </div>
+    );
+}
+ 
+export default Person;
